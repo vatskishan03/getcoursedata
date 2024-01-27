921781c636b2b9f6af177a8f8e79191526e0ac40
Kishan <kishanvats2003@gmail.com>
2024-01-27 12:20:53 +0530

    Package

--- server/app.js
+++ server/app.js
@@ -14,9 +14,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'public', 'index.html')); 
 });
 
-
-
-
 app.get('/public/style.css', (req, res) => { 
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, '..','public', 'style.css')); 
