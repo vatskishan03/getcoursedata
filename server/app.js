8ea651751e78a135f00fc4abf72720ed515e42f9
Kishan <kishanvats2003@gmail.com>
2024-01-27 10:42:20 +0530

    This is commit

---
+++ server/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
+const path = require('path'); 
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'public', 'index.html')); 
+});
+
+
+
+
+app.get('/public/style.css', (req, res) => { 
+    res.setHeader('Content-Type', 'text/css');
+    res.sendFile(path.join(__dirname, '..','public', 'style.css')); 
+});
+
+
+app.post('/courses', (req, res) => {
+    const { company, difficulty, rating } = req.body;
+
+    const pythonProcess = spawn('python', ['./server/script.py', company, difficulty, rating]);
+
+    pythonProcess.on('error', (error) => {
+        console.error(`Error initiating Python script: ${error}`);
+    });
+
+    let result = '';
+
+    pythonProcess.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+
+    pythonProcess.stdout.on('end', () => {
+        console.log(result);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Python script error: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        if (code === 0) {
+            res.json(JSON.parse(result));
+        } else {
+            res.status(500).send('Error in Python script');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ server/script.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sys
+import json
+
+file = pd.read_csv('./server/excelfile.csv')
+
+
+def filter_df(input_df, company, difficulty, rating):
+    try:
+        filtered_data = input_df.loc[
+            (input_df['Company_Name'] == company) &
+            (input_df['Difficulty'] == difficulty) &
+            (input_df['Ratings'] >= rating)
+        ]
+        return filtered_data[['Course_Name', 'Ratings', 'Link']]
+    except KeyError:
+        print("Enter Correct compName")
+        return pd.DataFrame()
+
+
+company = sys.argv[1]
+difficulty = sys.argv[2]
+rating = float(sys.argv[3])
+
+filtered_data = filter_df(file, company, difficulty, rating)
+
+courses_list = filtered_data.to_dict(orient='records')
+
+for course in courses_list:
+    course['Link'] = course['Link']
+
+print(json.dumps(courses_list))
